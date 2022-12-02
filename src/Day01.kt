8b1f231e8f5c4e87c3e0cdbe50b0d39055113f9e
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-12-02 11:40:14 -0300

    Day 01 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
+    var elvesCalories: List<Int>
+
+    fun calculateElvesCalories(input: List<String>): List<Int> {
+        val calories = mutableListOf<Int>()
+        var item = 0
+        var sum = 0
+        while(item < input.size) {
+            if (input[item] == "") {
+                calories.add(sum)
+                sum = 0
+            } else {
+                sum += input[item].toInt()
+            }
+            item++
+        }
+        calories.add(sum)
+        return calories
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        elvesCalories = calculateElvesCalories(input)
+        return elvesCalories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        elvesCalories = calculateElvesCalories(input)
+        return elvesCalories.sorted().reversed().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
