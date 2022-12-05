be47232664d82d69a3b68e8f2d8877d24c42d24b
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-12-05 10:05:28 -0300

    Day 00 (template) modified

--- src/Day00.kt
+++ src/Day00.kt
@@ -9,11 +9,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 24000)
-    check(part2(testInput) == 45000)
+    val testInput = readInput("Day00_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
 
-    val input = readInput("Day01")
+    val input = readInput("Day00")
     println(part1(input))
     println(part2(input))
 }
