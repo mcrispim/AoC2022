d46c0aca8e4954e46f076380c1cd90a710c86e1c
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-12-22 13:41:55 -0300

    Day 10 in completed

--- src/Day10.kt
+++ src/Day10.kt
@@ -6,24 +6,27 @@ fun main() {
         val measures = listOf(20, 60, 100, 140, 180, 220)
         val strength = mutableListOf<Int>()
         var nextMeasure = 0
-        var cycle = 0
-        var signal = 1
+        var cycle = 1
+        var registerX = 1
         while (nextCommand <= input.lastIndex) {
-            val lastSignal = signal
+            val initialRegisterX = registerX
             when (val command = input[nextCommand]) {
                 "noop" -> {
                     cycle++
                 }
                 else -> {
                     cycle += 2
-                    signal += command.substringAfter(" ").toInt()
+                    registerX += command.substringAfter(" ").toInt()
                 }
             }
             nextCommand++
             if (cycle > measures[nextMeasure]) {
-                strength.add(measures[nextMeasure++] * lastSignal)
+                strength.add(measures[nextMeasure] * initialRegisterX)
             } else if (cycle == measures[nextMeasure]) {
-                strength.add(measures[nextMeasure++] * signal)
+                strength.add(measures[nextMeasure] * registerX)
+            }
+            if (cycle >= measures[nextMeasure]) {
+                nextMeasure++
             }
             if (strength.size == measures.size)
                 break
@@ -32,12 +35,37 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        var registerX = 1
+        var instruction = 0
+        var duration = 0
+        var increment = 0
+        for (line in 0..5) {
+            for (pixel in 0..39) {
+                if (duration == 0) {
+                    val command = input[instruction++]
+                    if (command == "noop") {
+                        duration = 1
+                        increment = 0
+                    } else {
+                        duration = 2
+                        increment = command.substringAfter(" ").toInt()
+                    }
+                }
+                duration--
+                val sprite = registerX - 1.. registerX + 1
+                print(if (pixel in sprite) "#" else ".")
+                if (duration == 0) {
+                    registerX += increment
+                }
+            }
+            println()
+        }
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 146)
+    check(part1(testInput) == 13140)
     check(part2(testInput) == 146)
 
     val input = readInput("Day10")
